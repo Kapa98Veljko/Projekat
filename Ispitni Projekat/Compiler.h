b675ddecba772c0faea93d3706a71f29b8a703ed
Вељко Илић <iv170611d@student.etf.bg.ac.rs>
2020-02-22 22:18:47 +0100

    Just a beginnig

---
+++ Ispitni Projekat/Compiler.h
@@ -0,0 +1,14 @@
+#ifndef __COMPILER_H_
+#define __COMPILER_H_
+
+class Compiler {
+
+public:
+	Compiler();
+protected:
+	//Moguca protected sekcija
+private:
+
+};
+
+#endif// ! __COMPILER_H_
---
+++ Ispitni Projekat/Machine.h
@@ -0,0 +1,15 @@
+#ifndef _MACHINE_H_
+#define _MACHINE_H_
+
+class Machine {
+
+public:
+	Machine();
+protected:
+	 //Moguca protected sekcija!
+private:
+	 //Moguca private sekcija!
+
+};
+
+#endif // ! _MACHINE_H_
